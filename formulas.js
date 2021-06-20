b460ea41e8efc270ca3c458e0733ec76aea1a883
sfrnnda <sotoquevedosofia@gmail.com>
2021-06-21 01:50:06 +0200

    sofia
    kk

--- formulas.js
+++ formulas.js
@@ -3,3 +3,9 @@ function masaMasa(gSoluto,gSolucion){
     let calculo = (gSoluto/gSolucion)*100;
     return calculo;
 }
+
+//Calculo masa volumen
+function masaVolumen(gSoluto,mmSolucion){
+    let calculo=(gSoluto/mmSolucion)*100
+    return calculo;
+}
